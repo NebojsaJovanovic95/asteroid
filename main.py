2ebe2a896ce05be42b62ab79e216e2f0bc010de3
Nebojsa Jovanovic <nebojsajovanovic95@gmail.com>
2026-01-08 18:14:41 -0500

    CH3L3

--- main.py
+++ main.py
@@ -28,13 +28,13 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        player.update(dt)
         screen.fill(
             "black"
         )
         player.draw(screen)
-        pygame.display.flip()
         dt = pygame.time.Clock().tick(60) / 1000
-
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
