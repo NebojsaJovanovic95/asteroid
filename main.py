f6ade1b00425076c71f48f01af5df76a39d74ec4
Nebojsa Jovanovic <nebojsajovanovic95@gmail.com>
2026-01-08 14:33:25 -0500

    CH2L4

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode(
         (
             SCREEN_WIDTH,
@@ -25,6 +27,7 @@ def main():
             "black"
         )
         pygame.display.flip()
+        dt = pygame.time.Clock().tick(60) / 1000
 
 
 if __name__ == "__main__":
